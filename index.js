58a67b7e6a19b9baae0e5735b607c12940d94367
Gino Desangles <blushingbox@yahoo.com>
2019-03-12 01:58:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  var foundItem = document.querySelector(selector)
+  return foundItem
+}
 
+function nestedTarget() {
+  var foundItem = document.querySelector('#nested .target')
+  return foundItem
+}
+
+function deepestChild() {
+  var foundItem = document.querySelectorAll('#grand-node div')
+  var foundItemA = foundItem[foundItem.length-1]
+  return foundItemA
+}
+
+function increaseRankBy(n) {
+  var foundItem = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < foundItem.length; i++) {
+    foundItem[i].innerHTML = parseInt(foundItem[i].innerHTML)+n
+  }
+  return foundItem
+}
